900bd9263d16d084f80a6b96d5ac3fdff6e7690f
michaelsmorgon <smorgon@int.pl>
2024-02-04 23:34:27 +0100

    fix: change fs to fs/promises for cd function

--- src/navigation/fs_navigation.js
+++ src/navigation/fs_navigation.js
@@ -1,7 +1,9 @@
 import path from 'node:path';
 import fs from 'node:fs';
+import fsPromises from 'fs/promises';
 import currentDirectory from '../directory/current_directory.js';
 import errorMsg from '../error/error_msg.js';
+import fileSystem from '../fs/file_system.js';
 
 class FSNavigation {
   list() {
@@ -38,22 +40,20 @@ class FSNavigation {
     currentDirectory.setCurrentDir(upDir);
   }
 
-  cd(newPath) {
-    fs.stat(newPath, (err, stat) => {
-      if (err === null && stat.isDirectory()) {
+  async cd(newPath) {
+    try {
+      newPath = fileSystem.checkFilePath(newPath);
+      const stat = await fsPromises.stat(newPath);
+      if (stat.isDirectory()) {
         currentDirectory.setCurrentDir(newPath);
       } else {
-        newPath = path.join(currentDirectory.getCurrentDir(), newPath);
-        fs.stat(newPath, (err, stat) => {
-          if (err === null && stat.isDirectory()) {
-            currentDirectory.setCurrentDir(newPath);
-          } else {
-            errorMsg.printInvalidInputMsg();
-            currentDirectory.currentDirMsg();
-          }
-        });
+        errorMsg.printInvalidInputMsg();
+        currentDirectory.currentDirMsg();
       }
-    });
+    } catch {
+      errorMsg.printInvalidInputMsg();
+      currentDirectory.currentDirMsg();
+    }
   }
 }
 
